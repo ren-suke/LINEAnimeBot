b81924e044f986557fbe7763dd3350bb6767058d
RenMatsushita <ren.git.x@gmail.com>
2020-07-12 11:54:17 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import line from '@line/bot-sdk';
+const server = express();
+
+const LINE_CONFIG = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+server.listen(process.env.PORT || 8000);
+server.post('/v1/webhook', line.middleware(LINE_CONFIG), (request, response, next) => {
+  response.sendStatus(200);
+  console.log(req.body);
+});

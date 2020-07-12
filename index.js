14a2cf5ba3f536bb498d2c95bfb4a99fdda016cd
RenMatsushita <ren.git.x@gmail.com>
2020-07-12 13:44:21 +0900

    implemented

--- index.js
+++ index.js
@@ -6,9 +6,44 @@ const LINE_CONFIG = {
   channelAccessToken: process.env.LINE_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
+const ANIME_API_BASEURL = "http://api.moemoe.tokyo/anime/v1/master/"
+const lineClient = new line.Client(LINE_CONFIG);
 
 express.listen(process.env.PORT || 8000);
-express.post('/v1/webhook', line.middleware(LINE_CONFIG), (request, response, next) => {
+express.post('/v1/webhook', line.middleware(LINE_CONFIG), async (request, response, next) => {
   response.sendStatus(200);
-  console.log(req.body);
+  const promises = [];
+  request.body.events.forEach((event) => {
+    if(event.type === "message" && event.message.type === "text") {
+      const messageText = event.message.text;
+      const yearAndCourPattern = /^\d{4}\s[1-4]{1}$/g;
+      if (yearAndCourPattern.test(messageText)) {
+        const fetchResult = await fetch(ANIME_API_BASEURL + messageText.replace(' ', '/'), {method: 'GET'});
+        const resultJson = await fetchResult.json();
+        let replyText = '';
+        resultJson.forEach((anime) => {
+          replyText += anime.title;
+          replyText += '\n';
+          replyText += anime.public_url;
+          replyText += '\v';
+        })
+        promises.push(lineClient.replyMessage(event.replyToken, {
+          type: "text",
+          text: replyText,
+        }));
+      } else {
+        promises.push(lineClient.replyMessage(event.replyToken, {
+          type: "text",
+          text: "正しい形式で入力してください！！ (例：2019 1)"
+        }));
+      };
+    };
+    Promise.all(promises)
+      .then((response) => {
+        console.log(response, ' - proceed');
+      }).catch((error) => {
+        console.log(error);
+      })
+  })
+
 });
